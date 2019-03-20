6050deb6be5ecf0f7b29aff32035796b6fe3dddc
Ashhwini <ashwinih378@gmail.com>
2019-03-20 09:36:05 +0530

    logger-dto-assignment

---
+++ src/main/java/com/stackroute/moviecruiser/controller/MovieController.java
@@ -0,0 +1,25 @@
+package com.stackroute.moviecruiser.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class MovieController {
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	@GetMapping("/movie")
+	public ResponseEntity<String> fetchAllMovies() {
+		logger.debug("This is a debug message");
+		logger.info("This is an info message");
+		logger.warn("This is a warn message");
+		logger.error("This is an error message");
+		return new ResponseEntity<String>("Success", HttpStatus.OK);
+	}
+}
